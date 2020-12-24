a86b8470392816258ea9b3832f3e762016b7a5e6
luckyraj810 <72134374+luckyraj810@users.noreply.github.com>
2020-12-24 10:00:09 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,44 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        });
+        console.log("8");
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+        console.log("9");
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+          
+        });
+        console.log("7");
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+            console.log("6");
+        })
+    }
+
+    
+}
